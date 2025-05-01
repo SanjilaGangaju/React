80df09bbbd9f786abc35ac6ad266a2dc97e683fd
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-05-01 08:47:29 +0545

    JSX and React State

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import Header from './Header.jsx';
-import Footer from  './Footer.jsx';
-import Food from './Food.jsx';
+
+import { useState } from "react";
 function App() {
-       return(
-        <>
-        <Header></Header>
-        <Food></Food>
-        <Footer/>
-        </>
-        
-       );
+  const [newItem, setNewItem]= useState("")
+  
+  return (
+    <>
+      
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input value={newItem} onChange={e=> setNewItem(e.target.value)} type="text" id="item" />
+          <button className="btn">Add Item</button>
+        </div>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+       <li>
+              <label>
+                     <input type="checkbox"/>
+                     Item1
+              </label>
+              <button className="btn btn-danger">Delete</button>
+       </li>
+       <li>
+              <label>
+                     <input type="checkbox"/>
+                     Item1
+              </label>
+              <button className="btn btn-danger">Delete</button>
+       </li>
+       <li>
+              <label>
+                     <input type="checkbox"/>
+                     Item1
+              </label>
+              <button className="btn btn-danger">Delete</button>
+       </li>
+      </ul>
+    </>
+  );
 }
 
-export default App
+export default App;
